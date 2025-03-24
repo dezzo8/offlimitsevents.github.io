ae1da8c84e446acc9baacf51e7f656bdbcfef52d
Nate Hicks <nhicksdesign@gmail.com>
2025-03-24 00:47:35 -0400

    Added mouse interaction

--- resources/js/script.js
+++ resources/js/script.js
@@ -14,10 +14,10 @@ window.onload = function() {
 
       var material = new Blotter.ChannelSplitMaterial();
 
-      material.uniforms.uApplyBlur.value = 1.0;
-      material.uniforms.uAnimateNoise.value = 1.0;
       material.uniforms.uOffset.value = 0.5;
-      material.uniforms.uRotation.value = 180;
+    material.uniforms.uRotation.value = 50;
+    material.uniforms.uApplyBlur.value = 1;
+    material.uniforms.uAnimateNoise.value = 0.3;
 
       var blotter = new Blotter(material, {
         texts : text
@@ -27,4 +27,11 @@ window.onload = function() {
       var scope = blotter.forText(text);
 
       scope.appendTo(elem);
+      document.onmousemove = moveIt;
+    function moveIt(event) {
+      material.uniforms.uRotation.value = (event.clientX * .25);
+      material.uniforms.uOffset.value = (event.clientY * 0.00025);
+    }
+
 };
+
