f6a1eb09ec14f27b0f6d124bf486ae4079810fb4
Nate Hicks <nhicksdesign@gmail.com>
2025-07-09 18:53:58 -0400

    Added new video hero

--- resources/js/script.js
+++ resources/js/script.js
@@ -5,42 +5,7 @@ window.onload = function() {
         return check;
       };
       if (check = true) {
-        function mainBlotter() {
-        var text = new Blotter.Text("OFF LIMITS", {
-           family : "'Montserrat', serif",
-           fill : "#000",
-           size : 150,
-           weight : 800,
-           spacing : 10,
-           paddingLeft : 100,
-           paddingRight : 100,
-           leading : 1,
-
-          });
-
-        var material = new Blotter.ChannelSplitMaterial();
-
-        material.uniforms.uOffset.value = 0.5;
-      material.uniforms.uRotation.value = 50;
-      material.uniforms.uApplyBlur.value = 1;
-      material.uniforms.uAnimateNoise.value = 0.3;
-
-        var blotter = new Blotter(material, {
-          texts : text
-        });
-
-        var elem = document.getElementById("distortion-text");
-        var scope = blotter.forText(text);
-
-        scope.appendTo(elem);
-        document.onmousemove = moveIt;
-
-
-      function moveIt(event) {
-        material.uniforms.uRotation.value = (event.clientX * .25);
-        material.uniforms.uOffset.value = (event.clientY * 0.00025);
-      }
-    };
+        
      function secondBlotter() {
         var text2 = new Blotter.Text("Step into the surreal.", {
            family : "'Montserrat', serif",
@@ -70,7 +35,7 @@ window.onload = function() {
     } else {
       
     }
-  mainBlotter();
+
   secondBlotter();
 
 };
